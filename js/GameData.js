dc2c9a78b36770d2bc8e55b40b16bfa6a8d32667
TheGoldenMan278 <snickersa9@gmail.com>
2026-04-29 18:59:29 -0500

    Getting npm build to work

--- js/GameData.js
+++ js/GameData.js
@@ -37,9 +37,9 @@ export var runningOnAndroid = false;
 if (window.location.href.includes("thegg.games")) {
 	window.location.replace("https://skudpaisho.com");
 }
-if (shouldRedirectIfNotHttps() && window.location.href.includes("http://")) {
-	window.location.replace(window.location.href.replace("http://", "https://"));
-}
+// if (shouldRedirectIfNotHttps() && window.location.href.includes("http://")) {
+// 	window.location.replace(window.location.href.replace("http://", "https://"));
+// }
 
 export function shouldRedirectIfNotHttps() {
 	return false;
