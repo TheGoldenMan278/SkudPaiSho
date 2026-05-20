8619bda9decd788d2c094e1d702634cb7f9b1522
TheGoldenMan278 <snickersa9@gmail.com>
2026-05-19 21:24:22 -0500

    First version with evaluation function

---
+++ js/ai/SkudAI_ChessHelp.js
@@ -0,0 +1,317 @@
+/* Skud AI Help */
+
+
+import { ACCENT_TILE, BASIC_FLOWER } from '../GameData';
+import { simpleCanonRules } from '../skud-pai-sho/SkudPaiShoRules';
+import {
+  ARRANGING,
+  HOST,
+  GUEST,
+  NotationPoint,
+  PLANTING,
+  RowAndColumn,
+} from '../CommonNotationObjects';
+import {
+  NON_PLAYABLE,
+  POSSIBLE_MOVE,
+} from '../skud-pai-sho/SkudPaiShoBoardPoint';
+import {
+  SkudPaiShoNotationBuilder,
+  SkudPaiShoNotationMove,
+} from '../skud-pai-sho/SkudPaiShoGameNotation';
+import { WAITING_FOR_ENDPOINT } from '../GameConstants';
+import { SkudPaiShoGameManager } from '../skud-pai-sho/SkudPaiShoGameManager';
+
+//
+// Contains helper functions to get all possible moves
+//
+export class SkudAiChessHelp {
+	constructor() {}
+
+	// =========================================================
+	// MOVE GENERATION FUNCTIONS
+	// =========================================================
+
+	/**
+	 * Get list of all possible moves from current game state.
+	 * @param {SkudPaiShoGameManager} thisGame - Copy of game state.
+	 * @param {string} player - Either "HOST" or "GUEST".
+	 * @returns {[SkudPaiShoNotationMove]} List of all possible moves.
+	 */
+	getPossibleMoves = function(thisGame, player) {
+		var moves = [];
+
+		this.addPlantMoves(moves, thisGame, player);
+		this.addArrangeMoves(moves, thisGame, player);
+
+		return moves;
+	};
+
+	// Add all plant moves to list of possible moves
+	addPlantMoves = function(moves, game, player) {
+		if (!this.isOpenGate(game)) {
+			return;
+		}
+
+		var tilePile = this.getTilePile(game, player);
+
+		// For each tile in player's tile reserve ("tile pile"), build Planting moves
+		for (var i = 0; i < tilePile.length; i++) {
+			var tile = tilePile[i];
+			if (tile.type === BASIC_FLOWER) {
+				// For each basic flower
+				// Get possible plant points
+				var convertedMoveNum = this.moveNum * 2;
+				game.revealOpenGates(player, tile, convertedMoveNum, true);
+				var endPoints = this.getPossibleMovePoints(game);
+
+				for (var j = 0; j < endPoints.length; j++) {
+					var notationBuilder = new SkudPaiShoNotationBuilder();
+					notationBuilder.moveType = PLANTING;
+
+					notationBuilder.plantedFlowerType = tile.code;
+					notationBuilder.status = WAITING_FOR_ENDPOINT;
+
+					var endPoint = endPoints[j];
+
+					notationBuilder.endPoint = new NotationPoint(this.getNotation(endPoint));
+					var move = notationBuilder.getNotationMove(this.moveNum, player);
+
+					game.hidePossibleMovePoints(true);
+
+					var isDuplicate = false;
+					for (var x = 0; x < moves.length; x++) {
+						if (moves[x].equals(move)) {
+							isDuplicate = true;
+						}
+					}
+
+					if (!isDuplicate) {
+						moves.push(move);
+					}
+				}
+			}
+		}
+	};
+
+	// Add all arrange moves to list of possible moves
+	addArrangeMoves = function(moves, game, player) {
+		var startPoints = this.getStartPoints(game, player);
+
+		for (var i = 0; i < startPoints.length; i++) {
+			var startPoint = startPoints[i];
+
+			game.revealPossibleMovePoints(startPoint, true);
+
+			var endPoints = this.getPossibleMovePoints(game);
+
+			for (var j = 0; j < endPoints.length; j++) {
+				var notationBuilder = new SkudPaiShoNotationBuilder();
+				notationBuilder.status = WAITING_FOR_ENDPOINT;
+				notationBuilder.moveType = ARRANGING;
+				notationBuilder.startPoint = new NotationPoint(this.getNotation(startPoint));
+
+				var endPoint = endPoints[j];
+
+				notationBuilder.endPoint = new NotationPoint(this.getNotation(endPoint));
+				var move = notationBuilder.getNotationMove(this.moveNum, player);
+
+				game.hidePossibleMovePoints(true);
+
+				var isDuplicate = false;
+				for (var x = 0; x < moves.length; x++) {
+					if (moves[x].equals(move)) {
+						isDuplicate = true;
+					}
+				}
+
+				if (!isDuplicate) {
+					moves.push(move);
+				}
+			}
+		}
+	};
+
+	/**
+	 * Enhance moves with bonus actions when they create harmonies.
+	 * For each move that creates a harmony, we add variants with bonus plant/arrange moves.
+	 * @param {SkudPaiShoGameManager} game - Copy of game state.
+	 * @param {[SkudPaiShoNotationMove]} moves - Original move list.
+	 * @returns {[SkudPaiShoNotationMove]} New move list with harmony bonus moves added.
+	 */
+	enhanceMovesWithBonusActions = function(game, moves) {
+		var enhancedMoves = [];
+		var opponent = this.getOpponent();
+
+		for (var i = 0; i < moves.length; i++) {
+			var move = moves[i];
+			enhancedMoves.push(move); // Always include the basic move
+
+			// Check if this move creates a harmony
+			var copyGame = game.getCopy();
+			copyGame.runNotationMove(move);
+
+			var harmonyBefore = game.board.harmonyManager.numHarmoniesForPlayer(this.player);
+			var harmonyAfter = copyGame.board.harmonyManager.numHarmoniesForPlayer(this.player);
+
+			// If move creates a harmony, add bonus action variants
+			if (harmonyAfter > harmonyBefore) {
+				// Add variants with bonus plant moves
+				var plantBonusVariants = this.generateBonusPlantVariants(copyGame, move);
+				enhancedMoves = enhancedMoves.concat(plantBonusVariants);
+
+				// Add variants with bonus arrange moves
+				var arrangeBonusVariants = this.generateBonusArrangeVariants(copyGame, move);
+				enhancedMoves = enhancedMoves.concat(arrangeBonusVariants);
+			}
+		}
+
+		return enhancedMoves;
+	};
+
+	/**
+	 * Generate bonus plant move variants for a given move.
+	 * Takes a move that creates harmony, and returns versions with bonus plants attached.
+	 */
+	generateBonusPlantVariants = function(game, baseMove) {
+		var variants = [];
+		var tilePile = this.getTilePile(game, this.player);
+		var plantableFlowers = [];
+
+		// Collect available basic flowers to plant
+		for (var i = 0; i < tilePile.length; i++) {
+			if (tilePile[i].type === BASIC_FLOWER) {
+				plantableFlowers.push(tilePile[i]);
+			}
+		}
+
+		if (plantableFlowers.length === 0) {
+			return variants; // No flowers left to plant
+		}
+
+		// For each plantable flower, generate placement variants
+		for (var f = 0; f < plantableFlowers.length; f++) {
+			var flower = plantableFlowers[f];
+			game.revealOpenGates(this.player, flower, this.moveNum * 2, true);
+			var endPoints = this.getPossibleMovePoints(game);
+
+			for (var j = 0; j < endPoints.length; j++) {
+				var endPoint = endPoints[j];
+				var variant = baseMove.clone ? baseMove.clone() : JSON.parse(JSON.stringify(baseMove));
+				
+				// Add bonus plant information
+				variant.bonusTileCode = flower.code;
+				variant.bonusEndPoint = "(" + this.getNotation(endPoint) + ")";
+				variant.fullMoveText = baseMove.fullMoveText + "+" + variant.bonusTileCode + variant.bonusEndPoint;
+
+				// Prefer center placements for bonuses
+				if (j < 3) { // Only add some of the closest options to avoid explosion
+					variants.push(variant);
+				}
+			}
+
+			game.hidePossibleMovePoints(true);
+		}
+
+		return variants;
+	};
+
+	/**
+	 * Generate bonus arrange move variants for a given move.
+	 * Takes a move that creates harmony, and returns versions with bonus arrangements attached.
+	 */
+	generateBonusArrangeVariants = function(game, baseMove) {
+		var variants = [];
+		var startPoints = this.getStartPoints(game, this.player);
+
+		// For each tile we can move, generate movement variants
+		for (var i = 0; i < startPoints.length && variants.length < 6; i++) {
+			var startPoint = startPoints[i];
+			game.revealPossibleMovePoints(startPoint, true);
+			var endPoints = this.getPossibleMovePoints(game);
+
+			for (var j = 0; j < endPoints.length; j++) {
+				var endPoint = endPoints[j];
+				var variant = baseMove.clone ? baseMove.clone() : JSON.parse(JSON.stringify(baseMove));
+				
+				// Add bonus arrange information
+				variant.bonusStartPoint = this.getNotation(startPoint);
+				variant.bonusEndPoint = "(" + this.getNotation(endPoint) + ")";
+				variant.fullMoveText = baseMove.fullMoveText + "+" + this.getNotation(startPoint) + variant.bonusEndPoint;
+
+				variants.push(variant);
+
+				// Limit variants to avoid too many options
+				if (variants.length >= 6) break;
+			}
+
+			game.hidePossibleMovePoints(true);
+		}
+
+		return variants;
+	};
+
+	// =========================================================
+	// UTILITY FUNCTIONS
+	// =========================================================
+
+	// Get list of all tiles yet to be placed by the player
+	getTilePile = function(game, player) {
+		var tilePile = (player === GUEST) ? game.tileManager.guestTiles : game.tileManager.hostTiles;
+		return tilePile;
+	};
+
+	// Check if there is at least one open gate
+	isOpenGate = function(game) {
+		var cells = game.board.cells;
+		for (var row = 0; row < cells.length; row++) {
+			for (var col = 0; col < cells[row].length; col++) {
+				if (cells[row][col].isOpenGate()) {
+					return true;
+				}
+			}
+		}
+	};
+	
+	// Get list of all open points on the board where a piece could move
+	getPossibleMovePoints = function(game) {
+		var points = [];
+		for (var row = 0; row < game.board.cells.length; row++) {
+			for (var col = 0; col < game.board.cells[row].length; col++) {
+				if (game.board.cells[row][col].isType(POSSIBLE_MOVE)) {
+					points.push(game.board.cells[row][col]);
+				}
+			}
+		}
+		return points;
+	};
+	
+	// Convert cell to RowAndColumn
+	getNotation = function(boardPoint) {
+		return new RowAndColumn(boardPoint.row, boardPoint.col).notationPointString;
+	};
+	
+	// Get list of all board points that have a piece owned by the player that is able to move (not accent tile, not drained or trapped)
+	getStartPoints = function(game, player) {
+		var points = [];
+		for (var row = 0; row < game.board.cells.length; row++) {
+			for (var col = 0; col < game.board.cells[row].length; col++) {
+				var startPoint = game.board.cells[row][col];
+				if (startPoint.hasTile()
+					&& startPoint.tile.ownerName === player
+					&& startPoint.tile.type !== ACCENT_TILE
+					&& !(startPoint.tile.drained || startPoint.tile.trapped)) {
+					
+					points.push(game.board.cells[row][col]);
+				}
+			}
+		}
+		return points;
+	};
+ 
+	// Get if opponent if "HOST" or "GUEST"
+	getOpponent = function() {
+		return this.player === GUEST ? HOST : GUEST;
+	};
+	
+}
--- js/ai/SkudAI_ChessStyle.js
+++ js/ai/SkudAI_ChessStyle.js
@@ -1,7 +1,7 @@
 /**
  * SkudChessAI - A strategic AI for Skud Pai Sho using Chess programming techniques
  *
- * Improvements over SkudStrategicAI:
+ * Improvements over SkudChessAI:
  * - 
  */
 
@@ -27,11 +27,14 @@ import {
 	SkudPaiShoNotationMove,
 } from '../skud-pai-sho/SkudPaiShoGameNotation';
 import { WAITING_FOR_ENDPOINT } from '../GameConstants';
+import { SkudPaiShoGameManager } from '../skud-pai-sho/SkudPaiShoGameManager';
+import { SkudAiChessHelp } from './SkudAI_ChessHelp';
 
 // Constructor
 export function SkudChessAI() {
 	this.player = null;
 	this.moveNum = 0;
+	this.helper = new SkudAiChessHelp();
 }
 
 //
@@ -49,193 +52,263 @@ SkudChessAI.prototype.setPlayer = function(playerName) {
 	this.player = playerName;
 };
 
+/**
+ * Main AI function to get best move to play.
+ * @param {SkudPaiShoGameManager} game - Copy of game state.
+ * @param {number} moveNum - Turn number of current move.
+ * @returns {SkudPaiShoNotationMove} Notation for best move found by AI.
+ */
 SkudChessAI.prototype.getMove = function(game, moveNum) {
+	// console.log("Chess AI V1", this.player)
 	this.moveNum = moveNum;
 
-	var moves = this.getPossibleMoves(game, this.player);
+	// Move 0: Strategic accent tile selection
+	if (moveNum === 0) return this.selectAccentTiles(game);
 
-	var randomIndex = Math.floor(Math.random() * moves.length);
-	var randomMove = moves.splice(randomIndex, 1)[0];
+	var moves = this.helper.getPossibleMoves(game, this.player);
+	if (moves.length === 0) return null;
 
-	return randomMove;
-};
+	// Enhance moves with harmony bonus actions where applicable
+	moves = this.helper.enhanceMovesWithBonusActions(game, moves);
 
-//
-// Helper functions to get all possible moves
-//
-SkudChessAI.prototype.getPossibleMoves = function(thisGame, player) {
-	var moves = [];
+	// Score all moves and find the best
+	var bestMove = null;
+	var bestScore = -Infinity;
 
-	if (this.moveNum === 0) {
-		/* First turn is Accent Tile selection */
-		this.addAccentSelectionMoves(moves, thisGame, player);
-	} else {
-		/* Get list of all Planting moves and all Arranging moves */
-		this.addPlantMoves(moves, thisGame, player);
-		this.addArrangeMoves(moves, thisGame, player);
-	}
+	for (var i = 0; i < moves.length; i++) {
+		var move = moves[i];
+		var score = this.evaluateMove(game, move);
 
-	return moves;
-};
+		// Immediate win detection
+		if (score >= 999999999) return move;
 
-SkudChessAI.prototype.addAccentSelectionMoves = function(moves, game) {
-	/* Status: Random, working
-	*/
+		// Add small random factor to break ties and add variety
+		score += Math.random() * 2;
 
-	var tilePile = this.getTilePile(game, this.player);
-
-	var availableAccents = [];
-
-	for (var i = 0; i < tilePile.length; i++) {
-		if (tilePile[i].type === ACCENT_TILE) {
-			availableAccents.push(tilePile[i]);
+		if (score > bestScore) {
+			bestScore = score;
+			bestMove = move;
 		}
 	}
 
-	// For now, get random accent tiles
-	var chosenAccents = [];
-
-	var length = (simpleCanonRules) ? 2 : 4;
-
-	for (var i = 0; i < length; i++) {
-		var chosenIndex = Math.floor(Math.random() * availableAccents.length);
-		var randomAccentTile = availableAccents.splice(chosenIndex, 1)[0];
-		chosenAccents.push(randomAccentTile.code);
-	}
-
-	var move = new SkudPaiShoNotationMove("0" + this.player.charAt(0) + "." + chosenAccents.join());
-	moves.push(move);
+	return bestMove;
 };
 
-SkudChessAI.prototype.addPlantMoves = function(moves, game, player) {
-	if (!this.isOpenGate(game)) {
-		return;
+/**
+ * Select accent tiles strategically instead of randomly.
+ * Gets a combo of all base accent types for variety
+ * TODO: Take opponent accent tile selection into account
+ */
+SkudChessAI.prototype.selectAccentTiles = function(game) {
+	return new SkudPaiShoNotationMove("0" + this.player.charAt(0) + "." + "W,K,R,B");
+};
+
+/**
+ * Evaluate a move by simulating it and scoring the resulting position.
+ */
+SkudChessAI.prototype.evaluateMove = function(game, move) {
+	var copyGame = game.getCopy();
+	copyGame.runNotationMove(move);
+
+	var score = 0;
+	var opponent = this.helper.getOpponent();
+
+	// === IMMEDIATE WIN/LOSS DETECTION ===
+
+	// Check for our win
+	if (copyGame.board.winners.includes(this.player)) {
+		return 999999999;
 	}
 
-	var tilePile = this.getTilePile(game, player);
+	// Check if opponent could win on their next turn (threat)
+	var opponentThreatLevel = this.detectOpponentThreats(copyGame, opponent);
+	if (opponentThreatLevel > 0) {
+		// Penalize moves that don't address threats
+		score -= opponentThreatLevel * 100;
+	}
 
-	// For each tile in player's tile reserve ("tile pile"), build Planting moves
-	for (var i = 0; i < tilePile.length; i++) {
-		var tile = tilePile[i];
-		if (tile.type === BASIC_FLOWER) {
-			// For each basic flower
-			// Get possible plant points
-			var convertedMoveNum = this.moveNum * 2;
-			game.revealOpenGates(player, tile, convertedMoveNum, true);
-			var endPoints = this.getPossibleMovePoints(game);
+	// === HARMONY EVALUATION ===
 
-			for (var j = 0; j < endPoints.length; j++) {
-				var notationBuilder = new SkudPaiShoNotationBuilder();
-				notationBuilder.moveType = PLANTING;
+	// Our harmonies
+	var harmonyBefore = game.board.harmonyManager.numHarmoniesForPlayer(this.player);
+	var harmonyAfter = copyGame.board.harmonyManager.numHarmoniesForPlayer(this.player);
+	var harmonyDelta = harmonyAfter - harmonyBefore;
 
-				notationBuilder.plantedFlowerType = tile.code;
-				notationBuilder.status = WAITING_FOR_ENDPOINT;
+	score += harmonyDelta * 30;
 
-				var endPoint = endPoints[j];
+	// Harmonies crossing center are more valuable
+	var centerHarmBefore = game.board.harmonyManager.getNumCrossingCenterForPlayer(this.player);
+	var centerHarmAfter = copyGame.board.harmonyManager.getNumCrossingCenterForPlayer(this.player);
 
-				notationBuilder.endPoint = new NotationPoint(this.getNotation(endPoint));
-				var move = notationBuilder.getNotationMove(this.moveNum, player);
+	if (centerHarmAfter > centerHarmBefore) {
+		score += 60;
+	}
 
-				game.hidePossibleMovePoints(true);
+	// Opponent's harmonies (disruption is good)
+	var oppHarmBefore = game.board.harmonyManager.numHarmoniesForPlayer(opponent);
+	var oppHarmAfter = copyGame.board.harmonyManager.numHarmoniesForPlayer(opponent);
+	var oppHarmDelta = oppHarmAfter - oppHarmBefore;
 
-				var isDuplicate = false;
-				for (var x = 0; x < moves.length; x++) {
-					if (moves[x].equals(move)) {
-						isDuplicate = true;
-					}
-				}
+	score -= oppHarmDelta * 25; // Penalize opponent gains, reward opponent losses
 
-				if (!isDuplicate) {
-					moves.push(move);
-				}
-			}
+	// === RING FORMATION ===
+
+	var surroundness = copyGame.board.getSurroundness(this.player);
+	var surroundnessBefore = game.board.getSurroundness(this.player);
+
+	// Building surroundness is important for ring victory
+	if (surroundness > surroundnessBefore) score += 15;
+
+	// If we have good surroundness, prioritize ring length
+	if (surroundness >= 3) {
+		var ringLengthBefore = game.board.harmonyManager.ringLengthForPlayer(this.player);
+		var ringLengthAfter = copyGame.board.harmonyManager.ringLengthForPlayer(this.player);
+
+		if (ringLengthAfter > ringLengthBefore) {
+			score += 25 + (ringLengthAfter * 5);
 		}
 	}
-};
 
-SkudChessAI.prototype.addArrangeMoves = function(moves, game, player) {
-	var startPoints = this.getStartPoints(game, player);
+	// === POSITION QUALITY ===
 
-	for (var i = 0; i < startPoints.length; i++) {
-		var startPoint = startPoints[i];
+	// Tiles in gardens (controlled territory)
+	var gardenTilesBefore = game.board.numTilesInGardensForPlayer(this.player);
+	var gardenTilesAfter = copyGame.board.numTilesInGardensForPlayer(this.player);
 
-		game.revealPossibleMovePoints(startPoint, true);
+	score += (gardenTilesAfter - gardenTilesBefore) * 8;
 
-		var endPoints = this.getPossibleMovePoints(game);
+	// Capturing opponent tiles
+	var oppTilesBefore = game.board.numTilesOnBoardForPlayer(opponent);
+	var oppTilesAfter = copyGame.board.numTilesOnBoardForPlayer(opponent);
 
-		for (var j = 0; j < endPoints.length; j++) {
-			var notationBuilder = new SkudPaiShoNotationBuilder();
-			notationBuilder.status = WAITING_FOR_ENDPOINT;
-			notationBuilder.moveType = ARRANGING;
-			notationBuilder.startPoint = new NotationPoint(this.getNotation(startPoint));
+	if (oppTilesAfter < oppTilesBefore) score += 12;
 
-			var endPoint = endPoints[j];
+	// === HARMONY POTENTIAL ===
 
-			notationBuilder.endPoint = new NotationPoint(this.getNotation(endPoint));
-			var move = notationBuilder.getNotationMove(this.moveNum, player);
+	// Evaluate tiles that could form harmonies in future moves
+	score += this.evaluateHarmonyPotential(copyGame, this.player) * 3;
 
-			game.hidePossibleMovePoints(true);
+	// === ENDGAME AWARENESS ===
 
-			var isDuplicate = false;
-			for (var x = 0; x < moves.length; x++) {
-				if (moves[x].equals(move)) {
-					isDuplicate = true;
-				}
-			}
+	// Check if approaching endgame (few tiles left)
+	var ourTilePile = this.helper.getTilePile(game, this.player);
+	var basicFlowersLeft = this.countBasicFlowers(ourTilePile);
 
-			if (!isDuplicate) {
-				moves.push(move);
-			}
+	// In endgame, harmonies crossing center matter most
+	if (basicFlowersLeft <= 2) score += centerHarmAfter * 40;
+
+	// === PLANTING BONUS ===
+
+	// Slight preference for planting to develop the position
+	if (move.moveType === PLANTING) {
+		score += 5;
+	}
+
+	// === HARMONY BONUS ACTIONS ===
+	// Big score boost for moves that have bonus actions attached
+	// These are moves that successfully chain combo actions
+	if (move.hasHarmonyBonus && move.hasHarmonyBonus()) {
+		score += 100; // Significant bonus for utilizing harmony extra actions
+		
+		// Extra bonus if the bonus action is a plant (develops board)
+		if (move.bonusTileCode && move.moveType === ARRANGING) {
+			score += 40; // Arranging with a bonus plant is very productive
 		}
 	}
+
+	return score;
 };
 
-SkudChessAI.prototype.getTilePile = function(game, player) {
-	var tilePile = (player === GUEST) ? game.tileManager.guestTiles : game.tileManager.hostTiles;
-	return tilePile;
+/**
+ * Detect if the opponent has threatening positions that could lead to a win.
+ * Returns a threat level (0 = no threat, higher = more dangerous)
+ */
+SkudChessAI.prototype.detectOpponentThreats = function(game, opponent) {
+	var threatLevel = 0;
+
+	// Check opponent's surroundness and ring progress
+	var oppSurroundness = game.board.getSurroundness(opponent);
+	if (oppSurroundness >= 4) {
+		threatLevel += 20;
+
+		var oppRingLength = game.board.harmonyManager.ringLengthForPlayer(opponent);
+		if (oppRingLength >= 6) {
+			threatLevel += 50; // Very close to ring victory
+		} else if (oppRingLength >= 4) {
+			threatLevel += 25;
+		}
+	}
+
+	// Check opponent's center-crossing harmonies (endgame threat)
+	var oppCenterHarm = game.board.harmonyManager.getNumCrossingCenterForPlayer(opponent);
+	var ourCenterHarm = game.board.harmonyManager.getNumCrossingCenterForPlayer(this.player);
+
+	if (oppCenterHarm > ourCenterHarm + 2) {
+		threatLevel += 15;
+	}
+
+	return threatLevel;
 };
 
- SkudChessAI.prototype.isOpenGate = function(game) {
+/**
+ * Evaluate the potential for future harmonies based on tile positions.
+ * Looks at tiles that are adjacent to empty spaces that could complete harmonies.
+ */
+SkudChessAI.prototype.evaluateHarmonyPotential = function(game, player) {
+	var potential = 0;
 	var cells = game.board.cells;
+
 	for (var row = 0; row < cells.length; row++) {
 		for (var col = 0; col < cells[row].length; col++) {
-			if (cells[row][col].isOpenGate()) {
-				return true;
+			var point = cells[row][col];
+			if (point.hasTile() && point.tile.ownerName === player) {
+				var tile = point.tile;
+				if (tile.type === BASIC_FLOWER || tile.type === SPECIAL_FLOWER) {
+					// Count adjacent empty spaces that could extend harmonies
+					potential += this.countAdjacentPotential(game, row, col, tile);
+				}
 			}
 		}
 	}
- };
- 
- SkudChessAI.prototype.getPossibleMovePoints = function(game) {
-	var points = [];
-	for (var row = 0; row < game.board.cells.length; row++) {
-		for (var col = 0; col < game.board.cells[row].length; col++) {
-			if (game.board.cells[row][col].isType(POSSIBLE_MOVE)) {
-				points.push(game.board.cells[row][col]);
+
+	return potential;
+};
+
+/**
+ * Count adjacent positions that could potentially form harmonies.
+ */
+SkudChessAI.prototype.countAdjacentPotential = function(game, row, col, tile) {
+	var potential = 0;
+	var directions = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+
+	for (var dir of directions) {
+		var newRow = row + dir[0];
+		var newCol = col + dir[1];
+
+		if (newRow >= 0 && newRow < game.board.cells.length &&
+			newCol >= 0 && newCol < game.board.cells[newRow].length) {
+
+			var adjPoint = game.board.cells[newRow][newCol];
+			// Empty playable space = potential
+			if (!adjPoint.hasTile() && adjPoint.types && !adjPoint.types.includes('NON_PLAYABLE')) {
+				potential += 1;
 			}
 		}
 	}
-	return points;
- };
- 
- SkudChessAI.prototype.getNotation = function(boardPoint) {
-	return new RowAndColumn(boardPoint.row, boardPoint.col).notationPointString;
- };
- 
- SkudChessAI.prototype.getStartPoints = function(game, player) {
-	var points = [];
-	for (var row = 0; row < game.board.cells.length; row++) {
-		for (var col = 0; col < game.board.cells[row].length; col++) {
-			var startPoint = game.board.cells[row][col];
-			if (startPoint.hasTile()
-				&& startPoint.tile.ownerName === player
-				&& startPoint.tile.type !== ACCENT_TILE
-				&& !(startPoint.tile.drained || startPoint.tile.trapped)) {
-				
-				points.push(game.board.cells[row][col]);
-			}
+
+	return potential;
+};
+
+/**
+ * Count basic flowers remaining in a tile pile.
+ */
+SkudChessAI.prototype.countBasicFlowers = function(tilePile) {
+	var count = 0;
+	for (var i = 0; i < tilePile.length; i++) {
+		if (tilePile[i].type === BASIC_FLOWER) {
+			count++;
 		}
 	}
-	return points;
- };
- 
+	return count;
+};
+
